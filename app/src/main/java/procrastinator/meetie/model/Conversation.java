f031cf6e213d9882765e08399ad726b9b923ec42
Shahriar-Rahi <30775361+Shahriar-Rahi@users.noreply.github.com>
2018-02-07 22:34:40 +0600

    Update Conversation.java

--- app/src/main/java/procrastinator/meetie/model/Conversation.java
+++ app/src/main/java/procrastinator/meetie/model/Conversation.java
@@ -4,5 +4,13 @@ import java.util.ArrayList;
 
 
 public class Conversation {
+    private ArrayList<Message> listMessageData;
+    public Conversation(){
+        listMessageData = new ArrayList<>();
+    }
+
+    public ArrayList<Message> getListMessageData() {
+        return listMessageData;
+    }
 
 }
