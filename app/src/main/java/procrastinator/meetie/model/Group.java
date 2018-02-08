548924bee5b2ba9dcd4298699c71d11c261c0bb6
NAKUL DEB NATH <31253293+NAKUL2@users.noreply.github.com>
2018-02-08 11:38:40 +0600

    Update Group.java
    group class

--- app/src/main/java/procrastinator/meetie/model/Group.java
+++ app/src/main/java/procrastinator/meetie/model/Group.java
@@ -2,5 +2,10 @@ package procrastinator.meetie.model;
 
 
 public class Group extends Room {
+    public String id;
+    public ListFriend listFriend;
 
+    public Group(){
+        listFriend = new ListFriend();
+    }
 }
