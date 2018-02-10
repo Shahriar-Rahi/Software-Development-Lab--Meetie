cf8afc71603194881d2075f4faeeba746c3bd362
Ameen Ruhul Mohammad <ameensunny242@gmail.com>
2018-02-10 20:44:59 +0600

    merge conflicts edited

--- app/src/main/java/procrastinator/meetie/model/Conversation.java
+++ app/src/main/java/procrastinator/meetie/model/Conversation.java
@@ -1,4 +1,4 @@
-<<<<<<< refs/remotes/origin/master
+
 package procrastinator.meetie.model;
 
 import java.util.ArrayList;
@@ -7,13 +7,4 @@ import java.util.ArrayList;
 public class Conversation {
 
 }
-=======
-package procrastinator.meetie.model;
 
-import java.util.ArrayList;
-
-
-public class Conversation {
-
-}
->>>>>>> Project Initialization from Branch Ruhul
